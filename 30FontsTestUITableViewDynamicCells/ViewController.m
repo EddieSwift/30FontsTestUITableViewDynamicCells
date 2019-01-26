7a95a24d040b656b7f0bc61b5557776e8924464b
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2019-01-26 23:16:26 +0200

    Apple Fonts
    UITableView Dynamic Cells

--- 30FontsTestUITableViewDynamicCells/ViewController.m
+++ 30FontsTestUITableViewDynamicCells/ViewController.m
@@ -12,11 +12,96 @@
 
 @end
 
+NSInteger count = 0;
+
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIEdgeInsets inset = UIEdgeInsetsMake(20, 0, 0, 0);
+
+    self.tableView.contentInset = inset;
+    self.tableView.scrollIndicatorInsets = inset;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    NSLog(@"numberOfSectionsInTableView");
+    
+    NSLog(@"UIFonts count = %lu", (unsigned long)[[UIFont familyNames] count]);
+    
+    return [[UIFont familyNames] count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    NSLog(@"numberOfRowsInSection %ld", (long)section);
+    
+    NSArray *familyNames = [UIFont familyNames];
+    
+    NSString *familyName = [familyNames objectAtIndex:section];
+    
+    NSArray * fontNames = [UIFont fontNamesForFamilyName:familyName];
+    
+    return [fontNames count];
+}
+
+- (nullable NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    
+    NSArray *familyNames = [UIFont familyNames];
+    
+    NSString *familyName = [familyNames objectAtIndex:section];
+    
+    return familyName;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSLog(@"cellForRowAtIndexPath {%ld, %ld}", (long)indexPath.section, (long)indexPath.row);
+    
+    static NSString *identifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (!cell) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        NSLog(@"Cell created!");
+    } else {
+        
+        NSLog(@"Cell reused!");
+    }
+    
+    NSArray *familyNames = [UIFont familyNames];
+    
+    NSString *familyName = [familyNames objectAtIndex:indexPath.section];
+    
+    NSString *currentFamilyName = [familyNames objectAtIndex:indexPath.section];
+    
+    NSArray *fontNames = [UIFont fontNamesForFamilyName:familyName];
+    
+    NSString *fontName = [fontNames objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = fontName;
+    
+    UIFont *font = [UIFont fontWithName:fontName size:20];
+    
+    cell.textLabel.font = font;
+    
+    if (familyName != currentFamilyName) {
+        
+        count += [fontNames count];
+        NSLog(@"CountCurrentFonts = %lu, CountTotalFonts %ld", (unsigned long)[fontNames count], count);
+    }
+
+    
+//    cell.textLabel.text = [NSString stringWithFormat:@"Section = %ld, Row = %ld", (long)indexPath.section, (long)indexPath.row];
+    
+    return cell;
 }
 
 

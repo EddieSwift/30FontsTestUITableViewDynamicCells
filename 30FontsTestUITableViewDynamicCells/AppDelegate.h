a177769d04c3b09b15347913deb803f202028dbb
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2019-01-26 21:22:36 +0200

    Initial Commit

---
+++ 30FontsTestUITableViewDynamicCells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  30FontsTestUITableViewDynamicCells
+//
+//  Created by Eduard Galchenko on 1/26/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 30FontsTestUITableViewDynamicCells/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  30FontsTestUITableViewDynamicCells
+//
+//  Created by Eduard Galchenko on 1/26/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
